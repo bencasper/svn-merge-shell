c5ff0f1adff9985ea0c1fdfd62c6f44d7899fab8
Ben Casper <zsydn007@gmail.com>
2015-08-16 19:12:56 +0800

    merge svn with dic

--- svn-merge.sh
+++ svn-merge.sh
@@ -11,8 +11,8 @@ then
 fi
 echo "the file want to be merged is :"$1
 #0.define the root path of branches
-svn=http://svn2.letv.cn/tp/cms/letvcms/trunk/letvcms
-letvcms=/home/ben/IdeaProjects/cms-workspace/branches/letvcms/
+svn=
+letvcms=
 echo "path of svn trunk:"$svn
 echo "branches of workspace:"$letvcms
 
@@ -74,31 +74,7 @@ mergeFile $1 $svn $letvcms $fileDir $filePath
 
 #2.svn log --stop-on-copy filename
 
-:<<'COMMENT1'
-------------------------------------------------------------------------
-r1458 | zhangpeng | 2014-03-01 09:34:26 +0800 (Sat, 01 Mar 2014) | 1 line
-
-过滤板块内容的tab（\t),reviewer:zoulei id:3238
-------------------------------------------------------------------------
-r1124 | zhangpeng | 2014-02-20 15:44:19 +0800 (Thu, 20 Feb 2014) | 1 line
-
-板块内容、排行榜，reviewer:maoxinming id:2772
-------------------------------------------------------------------------
-r1006 | zhangpeng | 2014-02-17 16:15:11 +0800 (Mon, 17 Feb 2014) | 1 line
-
-板块内容标题显示板块id+板块名称、jax修改板块内容权重、板块内容编辑窗口关闭debug，reviewer:maoxinming id:2577
-------------------------------------------------------------------------
-r765 | lixiaochun | 2014-01-22 14:31:18 +0800 (Wed, 22 Jan 2014) | 1 line
-
-初始导入, reviewer:lixiaochun id:2117
-------------------------------------------------------------------------
-COMMENT1
-#set branches version(branchesVersion) of the file from the second row of the log output as 1458
-
 #3.svn update filename
-:<<'COMMENT2'
-At revision 1466.
-COMMENT2
 
 #set trunk new version(trunkVersion) of the file as 1466
 
